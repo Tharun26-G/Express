9c2298deac1b28fade07c7e9c97ad52290aaa3e8
Tharun26-G <tharun26.g@gmail.com>
2025-08-04 19:10:43 +0530

    multi-form data

--- Express7/index.js
+++ Express7/index.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const app = express();  
+import express from 'express';
+import multer from 'multer';
+
+const app = express(); 
+const upload = multer(); // for parsing multipart/form-data 
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(upload.array()); // for parsing multipart/form-data
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
