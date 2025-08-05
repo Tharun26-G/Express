632b7f2f891e26b0a2b6b16b1c8c590979ef6269
Tharun26-G <tharun26.g@gmail.com>
2025-08-05 20:18:31 +0530

    mongo connect with express

---
+++ Express8/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
+import { Person } from './models/Person.js';
+
+const app = express(); 
+const port = 3000;
+
+await connectDB();
+
+/* 
+ const MONGODB_URI = 'mongodb+srv://tharung:tharun123@cluster0.5hxwzfr.mongodb.net/express';
+
+    await mongoose.connect(MONGODB_URI).then(() => {
+    console.log("Database Connected");
+*/
+//removed and moved to config foldeer to simplify
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.post('/person',express.json(), async(req, res) => {
+  console.log(req.body);
+  //saving in the database
+  const { email, name, age } = req.body;
+  const newPerson = new Person({
+    name,
+    age,
+    email
+  })
+  await newPerson.save()
+  res.send("person added")
+  console.log(newPerson)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
